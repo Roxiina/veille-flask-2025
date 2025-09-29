b842845c6c7724c14608e57762972f5041bc82a4
Roxiina <flaviefumonde@gmail.com>
2025-09-29 15:41:05 +0400

    add

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/bonjour')
-def bonjour():
-    return "<h2>Bonjour à tous !</h2>"
 
 @app.route('/')
 def hello():
-    # Retour direct d'un code HTML
-    return "<h1>Hello, Flask!</h1><p>Bienvenue sur votre première page HTML servie directement !</p>"
+    return "<h1>Test HTML</h1>"
