04fa775e2a23cf5a0c6fd64b0546e63f7717f232
Roxiina <flaviefumonde@gmail.com>
2025-09-29 15:59:34 +0400

    fix

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask
+
+# Création de l'application Flask
 app = Flask(__name__)
 
+# Route principale
 @app.route('/')
 def hello():
-    return "<h1>Test HTML</h1>"
+    # HTML direct
+    return "Hello, Flask!"
+
+# Route secondaire
+@app.route('/bonjour')
+def bonjour():
+    return "<h1>Bonjour à tous !</h1><p>Voici une deuxième page servie directement par Flask.</p>"
+
+# Route index
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+# Route name
+@app.route('/user/<name>')
+def user(name):
+    return f"Hello, {name}"
+
+# Route
+@app.route('/perso/<name>')
+def user(name):
+    return render_template('index.html',username=name)
+# Point d'entrée
+if __name__ == "__main__":
+    # Active le mode debug pour rechargement automatique
+    app.run(debug=True)
